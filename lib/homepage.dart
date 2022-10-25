4d4d75fbdbf91ce57e130f57aadef7defc24089e
sanat-19 <sanatpanchal19@gmail.com>
2022-10-26 01:08:52 +0530

    Final Commit

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -9,28 +9,8 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  bool isSwitched = false;
-  var textValue = 'Switch is OFF';
-
-  void toggleSwitch(bool value) {
-    if (isSwitched == false) {
-      setState(() {
-        isSwitched = true;
-        textValue = 'Switch Button is ON';
-      });
-      print('Switch Button is ON');
-    } else {
-      setState(() {
-        isSwitched = false;
-        textValue = 'Switch Button is OFF';
-      });
-      print('Switch Button is OFF');
-    }
-  }
-
   @override
   Widget build(BuildContext context) {
-    print("Builder");
     return Scaffold(
       appBar: AppBar(title: const Text("Theme Change")),
       body: Center(
